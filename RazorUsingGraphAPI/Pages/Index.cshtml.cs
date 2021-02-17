bba2eba8b1c38828f21b2b22dc4a25ac3ff08ec6
Léon <admin@familieboers.onmicrosoft.com>
2021-02-17 19:23:39 +0100

    initial copy

---
+++ RazorUsingGraphAPI/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
+using RazorUsingGraphAPI.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorUsingGraphAPI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        ITokenAcquisition _tokenAcquisition;
+        public IndexModel(ILogger<IndexModel> logger, ITokenAcquisition tokenAcquisition)
+        {
+            _logger = logger;
+            _tokenAcquisition = tokenAcquisition;
+        }
+
+        public async Task OnGetAsync()
+        {
+
+        }
+    }
+}
