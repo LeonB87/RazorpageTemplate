b2e9c2457a2105831436c20e39f2f5c5dded10e6
Léon <admin@familieboers.onmicrosoft.com>
2021-02-27 17:34:03 +0100

    added Microsoft Login

---
+++ WebappAuthN/Graph/GraphConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebappAuthN.Graph
+{
+    public static class GraphConstants
+    {
+        // Defines the permission scopes used by the app
+        public readonly static string[] Scopes =
+        {
+            "User.Read",
+            "MailboxSettings.Read",
+            "Calendars.ReadWrite"
+        };
+    }
+}
--- WebappAuthN/Startup.cs
+++ WebappAuthN/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,11 +9,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using WebappAuthN.Data;
+using WebappAuthN.Graph;
 using WebappAuthN.Helpers;
 
 namespace WebappAuthN
@@ -29,6 +36,16 @@ namespace WebappAuthN
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            string ConnectionString = Configuration["SqlConnectionString"];
+            string AzureAdInstance = Configuration["AzureAdInstance"];
+            string AzureAdDomain = Configuration["AzureAdDomain"];
+            string AzureAdTenantId = Configuration["AzureAdTenantId"];
+            string AzureAdCallbackPath = Configuration["AzureAdCallbackPath"];
+            string AzureAdClientId = Configuration["AzureAdClientId"];
+            string AzureAdClientSecret = Configuration["AzureAdClientSecret"];
+
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -40,7 +57,7 @@ namespace WebappAuthN
                 .AddDefaultTokenProviders();
             services.AddRazorPages();
 
-            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1)
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
        .AddRazorPagesOptions(options =>
        {
            options.Conventions.AuthorizeAreaFolder("Identity", "/Account/Manage");
@@ -57,11 +74,117 @@ namespace WebappAuthN
             // using Microsoft.AspNetCore.Identity.UI.Services;
             services.AddSingleton<IEmailSender, EmailSender>();
 
-            services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+            services.AddAuthentication()
+            .AddMicrosoftAccount(microsoftOptions =>
             {
                 microsoftOptions.ClientId = Configuration["clientid"];
                 microsoftOptions.ClientSecret = Configuration["clientsecret"];
             });
+
+            //.AddMicrosoftIdentityWebApp(options =>
+            //{
+            //    Configuration.Bind("AzureAd", options);
+            //    options.SignInScheme = "OpenIdConnect";
+            //    options.Instance = AzureAdInstance;
+            //    options.Domain = AzureAdDomain;
+            //    options.ClientId = AzureAdClientId;
+            //    options.ClientSecret = AzureAdClientSecret;
+            //    options.TenantId = AzureAdTenantId;
+            //    options.CallbackPath = AzureAdCallbackPath;
+            //    options.Events ??= new OpenIdConnectEvents();
+            //    options.Prompt = "select_account";
+
+
+            //    options.Events.OnTokenValidated = async context =>
+            //    {
+            //        var tokenAcquisition = context.HttpContext.RequestServices
+            //            .GetRequiredService<ITokenAcquisition>();
+            //        var graphClient = new GraphServiceClient(
+            //            new DelegateAuthenticationProvider(async (request) =>
+            //            {
+            //                var token = await tokenAcquisition
+            //                    .GetAccessTokenForUserAsync(GraphConstants.Scopes, user: context.Principal);
+            //                request.Headers.Authorization =
+            //                    new AuthenticationHeaderValue("Bearer", token);
+            //            })
+            //        );
+
+            //        // Get user information from Graph
+            //        var user = await graphClient.Me.Request()
+            //        .Select(u => new
+            //        {
+            //            u.DisplayName,
+            //            u.Mail,
+            //            u.UserPrincipalName,
+            //            u.MailboxSettings
+            //        })
+            //        .GetAsync();
+
+            //        context.Principal.AddUserGraphInfo(user);
+
+            //        // Get the user's photo
+            //        // If the user doesn't have a photo, this throws
+            //        try
+            //        {
+            //            var photo = await graphClient.Me
+            //                .Photos["48x48"]
+            //                .Content
+            //                .Request()
+            //                .GetAsync();
+
+            //            context.Principal.AddUserGraphPhoto(photo);
+            //        }
+            //        catch (ServiceException ex)
+            //        {
+            //            if (ex.IsMatch("ErrorItemNotFound") ||
+            //                ex.IsMatch("ConsumerPhotoIsNotSupported"))
+            //            {
+            //                context.Principal.AddUserGraphPhoto(null);
+            //            }
+            //            else
+            //            {
+            //                throw;
+            //            }
+            //        }
+            //    };
+
+            //    options.Events.OnAuthenticationFailed = context =>
+            //    {
+            //        var error = WebUtility.UrlEncode(context.Exception.Message);
+            //        context.Response
+            //            .Redirect($"/Home/ErrorWithMessage?message=Authentication+error&debug={error}");
+            //        context.HandleResponse();
+
+            //        return Task.FromResult(0);
+            //    };
+
+            //    options.Events.OnRemoteFailure = context =>
+            //    {
+            //        if (context.Failure is OpenIdConnectProtocolException)
+            //        {
+            //            var error = WebUtility.UrlEncode(context.Failure.Message);
+            //            context.Response
+            //                .Redirect($"/Home/ErrorWithMessage?message=Sign+in+error&debug={error}");
+            //            context.HandleResponse();
+            //        }
+
+            //        return Task.FromResult(0);
+            //    };
+            //},"AzureAd").EnableTokenAcquisitionToCallDownstreamApi(options =>
+            //{
+            //    Configuration.Bind("AzureAd", options);
+            //}, GraphConstants.Scopes)
+
+            //// Add a GraphServiceClient via dependency injection
+            //.AddMicrosoftGraph(options =>
+            //{
+            //    options.Scopes = string.Join(' ', GraphConstants.Scopes);
+            //})
+
+            //// Use in-memory token cache
+            //// See https://github.com/AzureAD/microsoft-identity-web/wiki/token-cache-serialization
+            //.AddInMemoryTokenCaches()
+            //; 
 
             services.Configure<IdentityOptions>(options =>
             {
@@ -81,7 +204,11 @@ namespace WebappAuthN
                 // User settings.
                 options.User.AllowedUserNameCharacters =
                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-                options.User.RequireUniqueEmail = false;
+                options.User.RequireUniqueEmail = true;
+
+                // Sign-in settings.
+                options.SignIn.RequireConfirmedEmail = true;
+                options.SignIn.RequireConfirmedAccount = true;
             });
 
             services.ConfigureApplicationCookie(options =>
